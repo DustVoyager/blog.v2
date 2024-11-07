124af9f0eb70f99250e05c6dc56c1e8d9acdaf0d
DustVoyager <ohjamdol@gmail.com>
2024-11-07 16:48:12 +0900

    next-mdx-remote issue로 인한 nextConfig 옵션 추가

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  transpilePackages: ["next-mdx-remote"], // next-mdx-remote issue
 };
 
 export default nextConfig;
