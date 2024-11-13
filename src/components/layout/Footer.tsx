6c4969c916d51da06a9d6276f1234fb89a35d78a
DustVoyager <ohjamdol@gmail.com>
2024-11-13 23:13:49 +0900

    footer update

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,3 +1,7 @@
 export default function Footer() {
-  return <footer>© 2024. Dust Voyager</footer>;
+  return (
+    <footer className="py-8 items-center justify-center text-center text-xs text-gray-500 font-mono print:hidden">
+      © 2024. Dust Voyager
+    </footer>
+  );
 }
