6ceaf881d791bb9e98e1f7eb5f1521f0e90d2a4b
DustVoyager <ohjamdol@gmail.com>
2024-11-14 21:21:10 +0900

    header update

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,21 +1,44 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { cn } from "@/lib/utils";
+
+const navList = [
+  { name: "About", href: "/about" },
+  { name: "Blog", href: "/blog" },
+];
 
 export default function Header() {
+  const pathname = usePathname();
+
   return (
-    <header>
-      <h1>
-        <Link href="/">Dust Voyager</Link>
-      </h1>
-      <nav>
-        <ul>
-          <li>
-            <Link href="/about">About</Link>
-          </li>
-          <li>
-            <Link href="/blog">Blog</Link>
-          </li>
-        </ul>
-      </nav>
+    <header className="fixed w-full py-6 border-b bg-white z-10">
+      <div className="flex w-full max-w-[800px] mx-auto items-center justify-between">
+        <h1>
+          <Link href="/">오수빈 개발 블로그</Link>
+        </h1>
+        <nav>
+          <ul className="flex w-full items-center justify-between">
+            <li className="flex items-center gap-2">
+              {navList.map((navItem) => (
+                <Link
+                  href={navItem.href}
+                  key={navItem.name}
+                  className={cn(
+                    "rounded-full px-4 py-1 text-center text-sm",
+                    pathname?.startsWith(navItem.href)
+                      ? "bg-blue-600 text-white"
+                      : "text-primary"
+                  )}
+                >
+                  {navItem.name}
+                </Link>
+              ))}
+            </li>
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 }
--- src/components/post/PostListPage.tsx
+++ src/components/post/PostListPage.tsx
@@ -9,7 +9,7 @@ const PostListPage = async ({ category }: PostListProps) => {
   const postList = await getLatestPostList(category);
 
   return (
-    <section className="mx-auto my-12 w-full max-w-[800px] px-6">
+    <section className="mx-auto w-full max-w-[800px]">
       <div>
         <ul className="grid grid-cols-1 md:grid-cols-2 gap-12">
           {postList.map((post) => (
